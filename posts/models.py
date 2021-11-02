ea7cab614ac5c8739b9681b243d839de7fb2fba2
palza4dev <moonsj6940@gmail.com>
2021-11-02 15:47:38 +0900

    Modify: 초기세팅 일부 수정

--- posts/models.py
+++ posts/models.py
@@ -6,16 +6,31 @@ class Category(models.Model):
     name = models.CharField(max_length=30)
 
 class Post(models.Model):
-    title    = models.CharField(max_length=100)
-    content  = models.TextField()
-    category = models.CharField(max_length=50)
-    user     = models.ForeignKey(User, on_delete=models.CASCADE)
+    title      = models.CharField(max_length=100)
+    content    = models.TextField()
+    category   = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=False)
 
     class Meta:
         db_table = 'posts'
         
 class Comment(models.Model):
-    comment         = models.CharField(max_length=500)
-    user            = models.ForeignKey(User, on_delete=models.CASCADE)
-    post            = models.ForeignKey(Post, on_delete=models.CASCADE)
-    #parents_comment = models.ForeignKey()
+    comment    = models.CharField(max_length=500)
+    post       = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=False)
+    
+    class Meta:
+        db_table = 'comments'
+    
+class DetailComment(models.Model):
+    detail_comment = models.CharField(max_length=500)
+    user           = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment        = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    created_at     = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=False)
+    
+    class Meta:
+        db_table = 'detail_comments'
--- users/models.py
+++ users/models.py
@@ -4,8 +4,6 @@ class User(models.Model):
     name       = models.CharField(max_length=50)
     email      = models.EmailField(max_length=100, unique=True)
     password   = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=False)
 
     class Meta:
         db_table = 'users'
